836687a9bff3a21a922dcda3c976f8230f23ad67
vickyvaf <vickyadi243@gmail.com>
2022-07-20 18:16:05 +0700

    menambahkan alert

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-function enter() {
-    
-}
+function jawab() {
+    // document.getElementById("answer").innerHTML = "SALAH BRODI 😁";
+    // let jawaban = document.getElementById("jawab");
+    let jawaban = menjawab.value;
 
+    if (jawaban == "go" || jawaban == "Go" || jawaban == "GO" ||jawaban == "golang" || jawaban == "Golang" || jawaban == "GoLang" || jawaban == "GOLANG") {
+        Swal.fire({
+            icon: 'success',
+            title: 'Horee kamu benar!!!',
+            confirmButtonText: 'Lanjut',
+        })
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Salah, coba lagi yaa',
+            confirmButtonText: 'Ulangi',
+        });
+    }
+}
