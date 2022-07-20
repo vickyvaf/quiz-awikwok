3597e2aa6714924187f7854441c80a727519b10f
vickyvaf <vickyadi243@gmail.com>
2022-07-20 21:51:32 +0700

    add homepage

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function start () {
+    Swal.fire({
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_si9TVBZW8GlXjmukTRzSiQFxmPtk_xMn6HzWVauxuwv7iyjl',
+        imageWidth : 250,
+        title: 'Kamu yakin ingin menjawab quiz ini ?',
+        confirmButtonText: 'HAYUKK 😎',
+        confirmButtonColor: "#23C552",
+        showCancelButton: true,
+        cancelButtonColor : "#F84F31",
+        cancelButtonText: 'TYDACK 😣',
+        allowOutsideClick: false,
+        reverseButtons: true,
+    })
+}
+
 function jawab() {
     // document.getElementById("answer").innerHTML = "SALAH BRODI 😁";
     // let jawaban = document.getElementById("jawab");
@@ -16,12 +31,16 @@ function jawab() {
             icon: 'question',
             title: 'Kamu belum menjawab',
             confirmButtonText: 'Ulangi',
+            confirmButtonColor: "#1E2F97",
+            allowOutsideClick: false,
         })
     } else {
         Swal.fire({
             icon: 'error',
             title: 'Salah, coba lagi yaa',
             confirmButtonText: 'Ulangi',
+            confirmButtonColor: "#1E2F97",
+            allowOutsideClick: false,
         });
     }
 }
@@ -42,12 +61,27 @@ function jawab2() {
             icon: 'question',
             title: 'Kamu belum menjawab',
             confirmButtonText: 'Ulangi',
+            confirmButtonColor: "#1E2F97",
+            allowOutsideClick: false,
         })
     } else {
         Swal.fire({
             icon: 'error',
             title: 'Salah, coba lagi yaa',
             confirmButtonText: 'Ulangi',
+            confirmButtonColor: "#1E2F97",
+            allowOutsideClick: false,
         });
     }
+}
+
+function caraMain() {
+    Swal.fire({
+        imageUrl: 'https://cdn141.picsart.com/354073044035211.png?to=crop&type=webp&r=310x310&q=50',
+        imageWidth: 200,
+        text: 'Ngapain liat tutorial, tinggal main aja cuy 🤣 \n Game ini sangat mudah pake banget lochhh 😏',
+        confirmButtonText: 'Saya Mengerti',
+        confirmButtonColor: "#FFC100",
+        allowOutsideClick: false,
+    })
 }
